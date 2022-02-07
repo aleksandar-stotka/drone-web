30f57c59c4c08eb67e849e66cdfc37e7bb79cf8a
aleksandar angelevski <stotka123@yahoo.com>
2022-02-07 20:51:04 +0100

    work-on-redux

---
+++ src/components/teorija.js
@@ -0,0 +1,11 @@
+//1 createStore form redux
+
+//2 const store = createStore(reducer, initialStore)
+// store is a place where store our data
+//preku reducer gi promenuvame , reducer  eprv argument
+// potoa se pravi funccija reducer
+// reducer ima argument state, action
+//state = initialState a so action izmenuvame
+// toa go pravime so if state proveruvame dali akcijata so ja dispath e ednakva
+//akcijata ja potvrduvame so store.dispatch({type: "DECREASE"})
+//I PRAVIME nov object
