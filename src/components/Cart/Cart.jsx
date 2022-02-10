841ea31ad77d02c151dd1673507c2fbf1ff43699
aleksandar angelevski <stotka123@yahoo.com>
2022-02-10 15:11:50 +0100

    set-cart

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Container ,Typography, Button, Grid } from '@material-ui/core'
+import useStyles from "./styles"
+
+const Cart = ({cart}) => {
+    const isEmpty = !cart.line_items.len
+    const classes = useStyles()
+    const EmptyCart = () => (
+        <Typography variant="subtitle1" >You have no items in your shopping cart, start adding some!</Typography>
+    )
+
+    const FilledCart = () => (
+        <>
+            <Grid container spacing={3}> 
+                {cart.line_items.map((item) => (
+                    <Grid item xs={12} sm={4} key={item.id}>
+                        <div>{ item.name}</div>
+
+
+                    </Grid>
+                    
+                ))}
+            </Grid>  
+            <div className='classes.cardDetails'>
+                            <Typography variant='h4'>
+                                Subtotal: {cart.subtotal.formated_with_symbol}
+                            </Typography>
+                            <div>
+                    <Button className={classes.emptyButton} size="large" type="button" variant='contained' color='secondary'>
+                        
+                                </Button>
+                    <Button className={classes.checkoutButton} size="large" type="button" variant='contained' color='primary'>
+                        
+                                </Button>
+                            </div>
+                    </div>
+        
+        </>
+    )
+    return (
+        <Container>
+            <div className={classes.toolbar}></div>
+            <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
+            {isEmpty ? <EmptyCart/> : <FilledCart/> }
+
+        </Container>
+    )
+}
+
+
+export default Cart
