b9af317affdb36d2eb4b4c7e87718fdc2c32b734
aleksandar angelevski <stotka123@yahoo.com>
2022-02-28 16:28:23 +0100

    env-set

--- src/page/PageLogin.js
+++ src/page/PageLogin.js
@@ -3,7 +3,7 @@ import loginItems from "../data/login-items";
 import Photos from "../components/Photos";
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
-
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const PageLogin = () => {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
@@ -11,7 +11,7 @@ const PageLogin = () => {
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}?client_id=3Gt6SoE3Je2ZLKVlEad-Pxe8FRlpNMkvRkZfqa9vFs8`;
+    url = `${mainUrl}${clientID}`;
     try {
       const response = await fetch(url);
 
