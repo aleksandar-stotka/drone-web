fc29e1f4faa231c48d92c4fcc00eea38cdabf4d4
aleksandar angelevski <stotka123@yahoo.com>
2022-03-01 14:28:25 +0100

    set-slider-two

---
+++ src/components/videoSlider/VideoSlider.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./videoSlider.scss";
+import { useState, useEffect } from "react";
+import slide from "../../data/slide.js";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+
+const VideoSlider = () => {
+  const [people, setPeople] = useState(slide);
+  const [index, setIndex] = React.useState(0);
+
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex + 1;
+      if (index > people.length - 1) {
+        index = 0;
+      }
+      return index;
+    });
+  };
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex - 1;
+      if (index < 0) {
+        index = people.length - 1;
+      }
+      return index;
+    });
+  };
+
+  // useEffect(() => {
+  //   const lastIndex = people.length - 1
+  //   if (index < 0) {
+  //     setIndex(lastIndex)
+  //   }
+  //   if (index > lastIndex) {
+  //     setIndex(0)
+  //   }
+  // }, [index, people])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1;
+        if (index > people.length - 1) {
+          index = 0;
+        }
+        return index;
+      });
+    }, 3000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
+  return (
+    <section className="slide">
+      <div className="title"></div>
+      <div className="slide">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person" />
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default VideoSlider;
