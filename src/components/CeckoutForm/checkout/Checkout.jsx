7dd893e9c56a592e13f988c43a765994b479d31b
aleksandar angelevski <stotka123@yahoo.com>
2022-04-11 15:54:35 +0200

    set-search-input

--- src/components/CeckoutForm/checkout/Checkout.jsx
+++ src/components/CeckoutForm/checkout/Checkout.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { useEffect } from "react";
-import {useState} from 'react'
+import { useState } from "react";
 import AddressForm from "./AddressForm";
 import PaymentForm from "./PaymentForm";
 import { commerce } from "../../../lib/commerce";
 import { useGlobalContext } from "../../../context";
-
 
 import {
   CssBaseline,
@@ -22,75 +21,75 @@ import { Link, useHistory } from "react-router-dom";
 
 import useStyles from "./styles";
 
-    
 const steps = ["Shipping address", "Payment details"];
 
 const Checkout = ({ cart, hideCheck }) => {
-  
-  
- 
- 
-
   const classes = useStyles();
   const [activeStep, setActiveStep] = useState(0);
-  const [checkoutToken, setCheckoutToken] = useState(null)
-  const [shippingData, setShippingData] = useState({})
-  const [order, setOrder] = useState({})
-  const [card, setCart] = useState(cart)
-  const [errorMessage, setErrorMessage] = useState('')
-
- 
-
-  
-
-  
+  const [checkoutToken, setCheckoutToken] = useState(null);
+  const [shippingData, setShippingData] = useState({});
+  const [order, setOrder] = useState({});
+  const [card, setCart] = useState(cart);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     const generateToken = async () => {
       try {
-        const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' })
-        
-           console.log(token)
-        setCheckoutToken(token)
+        const token = await commerce.checkout.generateToken(cart.id, {
+          type: "cart",
+        });
 
-           
+        console.log(token);
+        setCheckoutToken(token);
       } catch (error) {
-         }
-    }
-    generateToken()
-    
-  }, [cart])
-   const handleCaptureCheckout = async (checkoutToken, newOrder) => {
-    try {
-      const incomingOrder = await commerce.checkout.capture(checkoutToken, newOrder)
-      setOrder(incomingOrder)
-      refreshCart()
-    } catch (error) {
-         setErrorMessage(error.data.error.message)
+        console.log(error);
       }
-  }
+    };
+    generateToken();
+  }, [cart]);
+  const handleCaptureCheckout = async (checkoutToken, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(
+        checkoutToken,
+        newOrder
+      );
+      setOrder(incomingOrder);
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
+  };
   const refreshCart = async () => {
     const newCart = await commerce.cart.refresh();
-    setCart(newCart)
-    
+    setCart(newCart);
+  };
 
-  }
-
-
-  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1)
-  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1)
-
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
 
   const next = (data) => {
-
-    setShippingData(data)
-    nextStep()
-
-  }
+    setShippingData(data);
+    nextStep();
+  };
 
   //////////
-               
-  const Form = () => activeStep === 0 ? <AddressForm checkoutToken={checkoutToken} next={next} hideCheck={hideCheck} /> : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} backStep={backStep} onCaptureCheckout={handleCaptureCheckout} nextStep={nextStep} />
+
+  const Form = () =>
+    activeStep === 0 ? (
+      <AddressForm
+        checkoutToken={checkoutToken}
+        next={next}
+        hideCheck={hideCheck}
+      />
+    ) : (
+      <PaymentForm
+        shippingData={shippingData}
+        checkoutToken={checkoutToken}
+        backStep={backStep}
+        onCaptureCheckout={handleCaptureCheckout}
+        nextStep={nextStep}
+      />
+    );
 
   const Confirmation = () => {
     return <div>Confirmation</div>;
@@ -110,10 +109,13 @@ const Checkout = ({ cart, hideCheck }) => {
                 <StepLabel>{step}</StepLabel>
               </Step>
             ))}
-             
           </Stepper>
           <hr></hr>
-          {activeStep === steps.length ? <Confirmation /> : checkoutToken   && <Form/>}
+          {activeStep === steps.length ? (
+            <Confirmation />
+          ) : (
+            checkoutToken && <Form />
+          )}
         </Paper>
       </main>
     </>
